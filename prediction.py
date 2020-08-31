37ed2d528cc62a2128a2c64b6199813aceaf160b
FreddoIsHere <fkelbel@ymail.com>
2020-08-31 20:50:15 +0200

    Automatically apply predictions to current positions

--- prediction.py
+++ prediction.py
@@ -1,17 +1,30 @@
 import argparse
 from model import Model
-from stats import *
 from api import Invest
+import time
+import sys
 
 parser = argparse.ArgumentParser(description='Run')
 parser.add_argument('--username', nargs="?", type=str, default='fredkelbel.fk@gmail.com', help='Username')
-parser.add_argument('--password', nargs="?", type=str, default='password', help='Password')
-parser.add_argument('--panel', nargs="?", type=str, default='Practice', help='Panel')
-parser.add_argument('--mode', nargs="?", type=str, default='Invest', help='Mode')
-parser.add_argument('--head', nargs="?", type=bool, default=True, help='Panel')
+parser.add_argument('--password', nargs="?", type=str, help='Password')
+parser.add_argument('--head', nargs="?", type=bool, default=False, help='Open browser')
 parser.add_argument('--ticker', nargs="?", type=str, help='Ticker')
 args = parser.parse_args()
+if args.ticker is None:
+    print("Retrieving current positions...")
+    invest = Invest(username=args.username, password=args.password, headless=not args.head)
+    tickers = invest.get_current_positions()
+else:
+    tickers = [args.ticker]
 model = Model()
-ticker = args.ticker
-prediction, confidence = model.predict_signal(ticker)
-print("Today's recommendation for {} is: {} with a confidence of {}%".format(ticker, prediction, confidence))
+for t in tickers:
+    start_time = time.time()
+    prediction, confidence = model.predict_signal(t)
+    print("Today's recommendation for {} is: {} with a confidence of {}%".format(t, prediction, confidence))
+    elapsed_time = time.time()
+    time_to_sleep = int(13 - (elapsed_time - start_time))
+    for i in range(time_to_sleep, 0, -1):  # only 5 api calls per minute allowed
+        sys.stdout.write("\r")
+        sys.stdout.write("Waiting time for next API call: {:2d}s".format(i))
+        sys.stdout.flush()
+        time.sleep(1)
